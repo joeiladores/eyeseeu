a72992280c24e4699f8647a0b5f4cefff21c84a3
Zafe123 <scottpilgrim712@gmail.com>
2022-12-23 18:54:51 +0800

    Blog Thumbanil with API

--- js/blog.js
+++ js/blog.js
@@ -1,38 +1,25 @@
-// function populateServicePage() {
+function populateTestimonialPage() {
+    fetch('https://638eb1de9cbdb0dbe31294ba.mockapi.io/blogsthumbnail')
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((user) => {
+                const cards = `
+          <div class="card col-8 mt-5 border-transaparent">
+        <img src="${user.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${user.name}</h5>
+        <p class="card-text">${user.text}</p>
+        <p class="card-text">${user.read}</p>
+      </div>
+    </div>
+        `;
+                document.querySelector('#card-container').innerHTML += cards;
+            });
+        });
+}
 
-//     // POPULATE MAIN SERVICE SECTION
-//     fetch("https://638eb1de9cbdb0dbe31294ba.mockapi.io/services")
-//     .then(response => response.json())
-//     .then(services => {
-//       console.log(services);
-         
-//       services.map(service => {
-  
-//         console.log(service);
-//         document.querySelector("#card-content").innerHTML = 
-//         `
-//           <section>
-//             <div class="page-title">
-//               <h1>${service.main_title}</h1>
-//             </div>
-//           </section>    
-  
-//           <section>
-//             <div class="parallax">
-//               <h1>${service.main_subtitle1}</h1>
-//               <h3>${service.main_subtitle2}</h3>
-//             </div>
-//           </section>    
-  
-//           <section>
-//             <div class="parallax-content">${service.main_content}</div>
-//           </section> 
-//         `;
-//       });    
-  
-//     });
+window.onload = function () {
+    populateTestimonialPage();
+};
 
-  
-    
-//     }
-  
+
