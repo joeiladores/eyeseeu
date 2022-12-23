1ddb9fae8085e9075d98637aa24e2b374db9afbf
Zafe123 <scottpilgrim712@gmail.com>
2022-12-23 23:21:38 +0800

    API thumbnail still not finished

--- js/blog.js
+++ js/blog.js
@@ -1,15 +1,16 @@
 function populateTestimonialPage() {
-    fetch('https://638eb1de9cbdb0dbe31294ba.mockapi.io/blogsthumbnail')
+    fetch('https://638eb1de9cbdb0dbe31294ba.mockapi.io/blogs')
         .then((response) => response.json())
         .then((data) => {
             data.forEach((user) => {
                 const cards = `
           <div class="card col-8 mt-5 border-transaparent">
-        <img src="${user.image}" class="card-img-top" alt="...">
+        <img src="${user.thumbnail}" class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title">${user.name}</h5>
-        <p class="card-text">${user.text}</p>
-        <p class="card-text">${user.read}</p>
+        <h5 class="card-title">${user.title}</h5>
+        <p class="card-text">${user.preview_content}</p>
+        <p class="card-text">${user.publish_date}</p>
+        <button type="button" class="btn btn-info">Read More</button>
       </div>
     </div>
         `;
