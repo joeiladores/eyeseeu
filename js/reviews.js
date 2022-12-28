3c34d13fd351598ddbe7f93453d4789d37d64ff0
Lemuelyy <al.claridad@gmail.com>
2022-12-28 17:13:30 +0800

    star rating fixed

--- js/reviews.js
+++ js/reviews.js
@@ -6,7 +6,6 @@ function populateTestimonialPage() {
     .then((response) => response.json())
     .then((data) => {
       data.forEach((user) => {
-        let rating = user.rating;
         // console.log(user.id);
         // for (let x = 0; x < user.id; x++) {
         // //   for (let y = 0; y < rating; y++) {
@@ -14,6 +13,11 @@ function populateTestimonialPage() {
         // //       '<i class="fa-solid fa-star"></i>';
         // //   }
         // // }
+        let ratings = Math.floor(user.rating);
+        let stars = '';
+        for (let i = 0; i < ratings; i++) {
+          stars += '<i class="fa-solid fa-star"></i>';
+        }
         const cards = `
         <div class="col-md-4 d-grid gy-4">
           <div class="card shadow border-0">
@@ -23,7 +27,7 @@ function populateTestimonialPage() {
               <hr>
               <h6 class="card-text">"${user.text}"</h6>
               <p class="card-text">
-              <div id="stars" class="text-muted">Rated ${user.rating} stars</div>
+              <div id="stars" class="text-muted">Ratings: <span style="color: #fef837">${stars}</span></div>
               </p>
             </div>
           </div>
