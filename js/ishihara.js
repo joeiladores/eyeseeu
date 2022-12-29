c5efa429fdb111701252d5015cb98f080c97671c
joeiladores <joelynladores@gmail.com>
2022-12-29 13:55:39 +0800

    Fetched Ishihara Plates and Information from Firestore and Display each plates

--- js/ishihara.js
+++ js/ishihara.js
@@ -32,6 +32,7 @@ const db = getFirestore(app);
 
 // Collection Reference
 const pageRef = collection(db, 'ishihara-page');
+const platesRef = collection(db, 'ishihara-vcd-38');
 
 // GET REAL TIME COLLECTION DATA
 onSnapshot(pageRef, (snapshot) => {
@@ -69,33 +70,130 @@ function populateInstruction(inst) {
     `
     <h2 class="text-center">${inst.instruction_title}</h2>
     ${inst.instruction}
-    ${inst.notes}    
+    ${inst.notes} 
+    <a id="startBtn" class="btn btn-primary mb-3">Start Test</a>   
   `;
+  document.getElementById("startBtn").addEventListener("click", startTest);
+  // console.log(document.querySelector(".instruction"));
 }
 
-const ishiharaRef = collection(db, 'ishihara-vcd-38');
+function displayTest() {
+  document.querySelector(".ishihara-test").style.display = "block";
+}
 
-// GET REAL TIME COLLECTION DATA
-const q = query(ishiharaRef, orderBy("plate", "asc"));
-onSnapshot(q, (snapshot) => {
-  let plates = [];
-  snapshot.docs.forEach((doc) => {
-    plates.push({ ...doc.data(), id: doc.id })
+function displayPlates(plate) {
+  // console.log("Display Plate");
+  // console.log(plate);
+
+  let progressValue = Math.floor((plate.plate / 38) * 100);
+
+  // FETCH AND DISPLAY PROGRESS BAR
+  document.querySelector(".progress").innerHTML =
+    `
+    <div class="progress-bar" role="progressbar" aria-label="Progress bar" style="width: ${progressValue}%" aria-valuenow="${progressValue}"
+    aria-valuemin="0" aria-valuemax="100"></div>
+  `
+
+  // FETCH AND DISPLAY PLATE NUMBER ND IMAGE
+  document.querySelector(".plate-container").innerHTML =
+    `
+    <div class="plate-name my-2 text-center">
+      <h5>Plate ${plate.plate}</h5>
+    </div>
+    <img class="ishihara-plate img-fluid" src="${plate.plateURL}"
+    " alt="Ishihara Plate ${plate.plate}" />
+  `
+
+  // FETCH AND DISPLAY OPTIONS
+  // console.log(plate.options);
+  let optionsElement = "", option = "", temp = "";
+  for (let i = 0; i < plate.options.length; i++) {
+    option = plate.options[i];
+    // console.log(option);
+    // console.log(typeof option)
+    // TODO: CATCH ERROR IN OPTION I DONT KNOW AND ONCLICK TO SPACES NOT ON
+    if (option == "I don't know") {
+      temp = "nothing"
+      console.log(`temp: ${temp}`);
+
+      optionsElement += `<button type="button" class="btn btn-primary" data-option="nothing" data-selected=false>${temp}</button>`;
+    }
+    else {
+      optionsElement += `<button type="button" class="btn btn-primary" data-option=${option} data-selected=false>${option}</button>`;
+    }
+  }
+  // console.log(optionsElement);
+  document.querySelector(".options").innerHTML =
+    `
+    ${optionsElement}
+    <button id="nextBtn" type="button" class="btn btn-dark" data-option="next">Next</button>
+  `
+}
+
+function startTest() {
+
+  let counter = 0;
+  let answer = [];
+  let selectedOption = "", previousOption = "";
+
+  displayTest();
+
+  // FETCH PLATES FROM FIRESTORE
+  const q = query(platesRef, orderBy("plate", "asc"));
+  onSnapshot(q, (snapshot) => {
+    const plates = [];
+    snapshot.docs.forEach((doc) => {
+      plates.push({ ...doc.data(), id: doc.id });
+    });
+
+    // DISPLAY INITIAL PLATE 1
+    displayPlates(plates[0]);
+
+    // ADD EVENT LISTENERS TO OPTIONS
+    document.querySelector(".options").addEventListener("click", (e) => {
+      e.preventDefault();
+
+      let targetElement = e.target;
+      let option = targetElement.dataset.option;
+      let parentElement = document.querySelector(".options");
+
+      // console.log(targetElement);
+      console.log(`Onclick: ${option}`);
+
+      if (option === "next" && selectedOption != "") {
+        console.log(`Selected answer: ${selectedOption}`);
+        counter++;
+        // PUSH SELECTED OPTION TO THE ANSWER ARRAY
+        answer.push(selectedOption);
+        console.log(`Pushed to answer[]: ${selectedOption}`);
+        console.log(`Answer[]: ${answer}`);
+      }  
+      else if (option === "next" && selectedOption === "") {
+        alert("Please select answer");
+      }
+      else {
+        // ONLY CLICK ON BUTTON OPTIONS AND NOT THE SPACES OUTSIDE THE ELEMENT
+        if (targetElement.type === "button") {
+          console.log("Clicked on a button");
+          selectedOption = option;
+          console.log(`Selected option: ${selectedOption}`);
+          targetElement.dataset.selected = true;
+          targetElement.classList.add("active");
+          // TODO: FIX BUTTON COLOR ACTIVE
+        }
+      }
+
+      // DISPLAY NEZXT PLATE EVERY CLICK ON NEXT BUTTON
+      displayPlates(plates[counter]);
+
+      if (counter === 37) {
+        console.log("End of plates");
+        // TODO: COMPUTE RESULTS AND DISOPLAY AS RESPONSIVE TABLE
+      }
+
+    });
 
   });
 
-  // console.log(plates.length);
-  // console.log(plates);
 
-  ishiharaStartTest(plates);
-});
-
-function ishiharaStartTest(plates) {
-
-  
-
-
-
-  console.log(plates.length);
-  console.log(plates);
 }
