7e774dbfa1e89703725524476de109ea14c40f91
Zafe123 <scottpilgrim712@gmail.com>
2023-01-02 11:17:19 +0800

    Design for About us and Blogs

--- js/blog.js
+++ js/blog.js
@@ -4,19 +4,51 @@ function populateTestimonialPage() {
         .then((data) => {
             data.forEach((user) => {
                 const cards = `
-          <div class="card col-8 mt-5 border-transaparent">
-        <img src="${user.thumbnail}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${user.title}</h5>
-        <p class="card-text">${user.preview_content}</p>
-        <p class="card-text">${user.publish_date}</p>
-        <button type="button" class="btn btn-info">Read More</button>
-      </div>
-    </div>
+                <div class="card mt-3" style="max-width: 950px;">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="${user.Thumbnail}" class="img-fluid rounded-start mt-3 ms-3 mb-3" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body ms-3">
+                      <h5 class="card-title">${user.Title}</h5>
+                      <p class="card-text">${user.Content_Preview}</p>
+                      <p class="card-text">${user.Publish_Date}</p>
+                      <button type="button" class="btn btn-info">Read More</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
         `;
                 document.querySelector('#card-container').innerHTML += cards;
             });
         });
+}
+
+function GetData() {
+
+  fetch('https://638eb1de9cbdb0dbe31294ba.mockapi.io/blogs')
+      .then( response =>
+      {
+          if ( response.status !== 200 )
+          {
+              console.log( 'Looks like there was a problem. Status Code: ' +
+                  response.status );
+              return;
+          }
+
+          console.log( response.headers.get( "Content-Type" ) );
+          return response.json();
+      }
+      )
+      .then( myJson =>
+      {
+          console.log( JSON.stringify( myJson ) );
+      } )
+      .catch( err =>
+      {
+          console.log( 'Fetch Error :-S', err );
+      } );
 }
 
 window.onload = function () {
