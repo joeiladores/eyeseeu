4bf39ba6534d5da0b4e18b7486da0476e2a5dc94
FinTo0322 <finsienes@gmail.com>
2023-01-13 03:47:56 +0800

    already fetch data but no proper design

--- js/product.js
+++ js/product.js
@@ -1,28 +1,20 @@
-// FIREBASE
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+function displayProduct() {
+  fetch('https://63bec3bce348cb076217c92f.mockapi.io/products?p=1&l=10')
+      .then((response) => response.json())
+      .then((data) => {
+          data.forEach((user) => {
+              const cards = `
+              <div class="col-2 card m-5 links" style="width: 17rem; height: 20.5em;" >
+              <img src="${user.img}" alt="case" class="card-img-top img-fluid">
+              <h6 class="text-center card-text">${user.name}</h3>
+            </div>
+      `;
+              document.querySelector('#productDetails').innerHTML += cards;
+          });
+      });
+}
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBkgjSTL62v09kT4HMoIuSl0MVNXZfqVTQ",
-  authDomain: "products-6e252.firebaseapp.com",
-  databaseURL: "https://products-6e252-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "products-6e252",
-  storageBucket: "products-6e252.appspot.com",
-  messagingSenderId: "256757194700",
-  appId: "1:256757194700:web:984ed9a512c5d9c4520769",
-  measurementId: "G-XS22D8CWC3"
+window.onload = function () {
+  displayProduct();
 };
-
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-import { getDatabase, ref, query, orderByChild } from "firebase/database";
-import { getAuth } from "firebase/auth";
-
-const db = getDatabase();
-const auth = getAuth();
-
-const myUserId = auth.currentUser.uid;
-const topUserPostsRef = query(ref(db, 'user-posts/' + myUserId), orderByChild('starCount'));
-
+a
