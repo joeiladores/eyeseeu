4fd3420972abee03c8230ca23e98e3c6482b232c
FinTo0322 <finsienes@gmail.com>
2023-01-18 12:32:07 +0800

    last minute fix

--- js/product.js
+++ js/product.js
@@ -13,49 +13,48 @@ fetch('https://63bec3bce348cb076217c92f.mockapi.io/products')
 
 
 // product display
-window.onload = function () {
-  displayProduct();
-};
 
-function displayProduct() {
+
+// function displayProduct() {
   
-  fetch('https://63bec3bce348cb076217c92f.mockapi.io/products?page=1&limit=15')
-      .then((response) => response.json())
-      .then((data) => {
-        document.getElementById("loader").style.display = "block";
-        let productDetails = document.querySelector('#productDetails');
-          productDetails.innerHTML = "";
-          let randomData = [];
-          while (data.length > 0) {
-              let randomIndex = Math.floor(Math.random() * data.length);
-              randomData.push(data[randomIndex]);
-              data.splice(randomIndex, 1);
-          }
-          randomData.forEach((prod) => {
-              const cards =  
-              `<div class="col-md-4 g-3 text-center">
-          <figure class="card card-product-grid" >
-          <a class="nav-link" data-name="${prod.name}">
-            <div class="img-wrap"> 
-              <img src="${prod.image1}" class="img-fluid">
-              <img src="${prod.img2}" class="img-top img-fluid">
-            </div>
-            <figcaption class="info-wrap">
-              <div class="fix-height">
-                <h5 class="title">${prod.name}</h5>
-              </div>
-            </figcaption>
-            </a>
-          </figure>
-        </div>`;
-              document.querySelector('#productDetails').innerHTML += cards;
+//   fetch('https://63bec3bce348cb076217c92f.mockapi.io/products?page=1&limit=15')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         document.getElementById("loader").style.display = "block";
+//         let productDetails = document.querySelector('#productDetails');
+//           productDetails.innerHTML = "";
+//           let randomData = [];
+//           while (data.length > 0) {
+//               let randomIndex = Math.floor(Math.random() * data.length);
+//               randomData.push(data[randomIndex]);
+//               data.splice(randomIndex, 1);
+//           }
+//           randomData.forEach((prod) => {
+//               const cards =  
+//               `<div class="col-md-4 g-3 text-center">
+//           <figure class="card card-product-grid" >
+//           <a class="nav-link" data-name="${prod.name}">
+//             <div class="img-wrap"> 
+//               <img src="${prod.image1}" class="img-fluid">
+//               <img src="${prod.img2}" class="img-top img-fluid">
+//             </div>
+//             <figcaption class="info-wrap">
+//               <div class="fix-height">
+//                 <h5 class="title">${prod.name}</h5>
+//               </div>
+//             </figcaption>
+//             </a>
+//           </figure>
+//         </div>`;
+//               document.querySelector('#productDetails').innerHTML += cards;
               
-          });
-          document.getElementById("loader").style.display = "none";
+//           });
+          
+//           document.getElementById("loader").style.display = "none";
 
-      });
+//       });
 
-}
+// }
 // end of product display
 
 
